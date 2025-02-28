027d3a66411e2fa191f595adfe25fea4e823c973
dkirby-ms <saitcho@outlook.com>
2025-02-27 22:20:06 -0500

    stuff

--- webmud_client/components/game/ActionPanel.tsx
+++ webmud_client/components/game/ActionPanel.tsx
@@ -1,15 +1,19 @@
 "use client";
 import { useSession } from "next-auth/react";
-import { useEffect } from "react";
-import { Flex, Box } from "@radix-ui/themes";
+import { useEffect, useRef } from "react";
+import { Flex, Box, Text, Strong } from "@radix-ui/themes";
 import { useGameService } from "../../contexts/GameServiceContext.tsx";
+import { StickToBottom } from 'use-stick-to-bottom';
 
 export function ActionPanel() {
 
     const { data: session } = useSession();
     const { gameState } = useGameService();
+    const messageEndRef = useRef<HTMLDivElement>(null);
+
     useEffect(() => {
         // Subscribe to the game state
+        //messageEndRef.current?.scrollIntoView({ behavior: 'smooth' });
     }, [gameState]);
 
     if (!session) return <div>Not authenticated</div>
@@ -17,9 +21,23 @@ export function ActionPanel() {
     if (!gameState) return <div>Loading...</div>
 
     return (
-        <Flex>
-            <Box>{gameState.room}</Box>
-            <Box>{gameState.roomDescription}</Box>
+        <Flex direction="column">
+            <Box><Strong>{gameState.room}</Strong></Box>
+            <Box><Text>{gameState.roomDescription}</Text></Box>
+            {gameState.gameMessages && (
+                <Box>
+                    <StickToBottom className="h-[50vh] relative" resize="smooth" initial="smooth">
+                        <StickToBottom.Content className="flex flex-col gap-4">
+                            {gameState.gameMessages.map((msg, index) => (
+                                <div key={index}>
+                                    {msg}
+                                </div>
+                            ))}
+                            <div ref={messageEndRef} />
+                        </StickToBottom.Content>
+                    </StickToBottom>
+                </Box>
+            )}
         </Flex>
     )
 };
